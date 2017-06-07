75c83da0675155469e6fce6d35085dcf0745ae42
Lukpoll <lukpoll@gmail.com>
2017-06-07 17:58:40 +0200

    Initial commit

---
+++ testapp1.py
@@ -0,0 +1,81 @@
+from flask import Flask, render_template, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import statistics
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///baza1.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'True'
+
+db = SQLAlchemy(app)
+
+class Formdata (db.Model):
+    __tablename__='baza1'
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    wiek = db.Column(db.Integer)
+    plec = db.Column(db.Integer)
+    wielodzietnosc = db.Column(db.Integer)
+    wielmiasta = db.Column(db.Integer)
+    stukoncz = db.Column(db.Integer)
+    stzamiesz = db.Column(db.Integer)
+    strodzice = db.Column(db.Integer)
+    stgdzie = db.Column(db.String)
+    ilepok = db.Column(db.String)
+    ilewsplok = db.Column(db.String)
+    ilezmian = db.Column(db.Integer)
+    powzmian = db.Column(db.String)
+    order = db.Column(db.String)
+    znanewczesniej = db.Column(db.String)
+    koedukacyjne = db.Column(db.String)
+    gdzieszukane = db.Column(db.String)
+    gdzieszukanetxt = db.Column(db.String)
+    mpk = db.Column(db.Integer)
+    czasprzejazdu = db.Column(db.Integer)
+    rynekok = db.Column(db.Integer)
+
+
+    def __init__(self, wiek, plec, wielodzietnosc, wielmiasta, stukoncz, stzamiesz, strodzice, stgdzie, ilepok, ilewsplok, ilezmian, powzmian, order, znanewczesniej, koedukacyjne, gdzieszukane, gdzieszukanetxt, mpk, czasprzejazdu, rynekok, wnioski):
+
+        self.wiek = wiek
+        self.plec = plec
+        self.wielodzietnosc = wielodzietnosc
+        self.wielmiasta = wielmiasta
+        self.stukoncz = stukoncz
+        self.stzamiesz = stzamiesz
+        self.strodzice = strodzice
+        self.stgdzie = stgdzie
+        self.ilepok = ilepok
+        self.ilewsplok = ilewsplok
+        self.ilezmian = ilezmian
+        self.powzmian = powzmian
+        self.order = order
+        self.znanewczesniej = znanewczesniej
+        self.koedukacyjne = koedukacyjne
+        self.gdzieszukane = gdzieszukane
+        self.gdzieszukanetxt = gdzieszukanetxt
+        self.mpk = mpk
+        self.czasprzejazdu = czasprzejazdu
+        self.rynekok = rynekok
+        self.wnioski = wnioski
+
+db.create_all()
+
+
+@app.route("/")
+def index():
+    return render_template('base.html')
+
+@app.route("/form")
+def show_form():
+    return render_template('form.html')
+
+@app.route("/save", methods=['POST'])
+def add_todatabase():
+    return render_template('base.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
